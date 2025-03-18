141d44c4fdf923ac3247a68bef9c3f894a97abfc
Akshay Bhinge <akshay.bhinge@techholding.co>
2025-03-18 18:14:30 +0530

    config changes

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export", // Enable static exports
-  images: {
-    unoptimized: true, // Disable Image Optimization API
-  },
+//   output: "export", // Enable static exports
+//   images: {
+//     unoptimized: true, // Disable Image Optimization API
+//   },
   experimental: {
     serverActions: true,
     serverComponentsExternalPackages: ["better-sqlite3"],
